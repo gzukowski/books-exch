93e9fc1bd5b43090f6bfc74307d023f5c7b23e60
unknown <gracek_608@wp.pl>
2024-09-23 22:40:29 +0200

    simple project setup

---
+++ books/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
+    condition = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='uploads/product/', null=True)
+
+    def __str__(self):
+        return self.title
